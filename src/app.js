7e403a22deb4d26b3947a4ba95ebc9380274e599
Roy S. <bookingrstubbs@gmail.com>
2025-09-09 23:38:33 +0000

    RandomCardaGenerator-RoyS

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-import "bootstrap";
-import "./style.css";
+function generateCard() {
+  
+  let suits = ["hearts", "spades", "clubs", "diamonds"];
+  let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+  let suit = suits[Math.floor(Math.random() * suits.length)];
+  let value = values[Math.floor(Math.random() * values.length)];
+
+ 
+  let suitSymbols = {
+    hearts: "♥",
+    spades: "♠",
+    clubs: "♣",
+    diamonds: "♦"
+  };
+
+  
+  let cardDiv = document.getElementById("card");
+
+  cardDiv.className = "card " + suit;
+
+  cardDiv.innerHTML = `
+    <div class="corner top-left">${value}${suitSymbols[suit]}</div>
+    ${value}${suitSymbols[suit]}
+    <div class="corner bottom-right">${value}${suitSymbols[suit]}</div>
+  `;
+}
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = generateCard;
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
